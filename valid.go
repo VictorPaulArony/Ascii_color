adb0ccd97e7dc2fabb49b03f9b6673a95460b570
victorpaularony <smallypauls@gmail.com>
2024-06-11 15:19:16 +0300

    UPDATED: art.go file to be dynamic

--- valid.go
+++ valid.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"regexp"
 )
-// check if the color flag format is a valid format 
+
+// check if the color flag format is a valid format
 func IsColorFlagValid(mycolorflag string) bool {
 	match, err := regexp.MatchString(`^--color=([a-z]|[A-Z])+$`, mycolorflag)
 	if err != nil {
@@ -12,4 +13,21 @@ func IsColorFlagValid(mycolorflag string) bool {
 		return false
 	}
 	return match
+}
+
+func Filenamevalidate(m string) string {
+	if !filenameExist(m) {
+		return "FILE DOES NOT EXSIT OR CHECK THE SPELLING OF YOUR FILES (e.g standard.txt)"
+	}
+	if m == "shadow" || m == "shadow.txt" {
+		return "shadow.txt"
+	} else if m == "thinkertoy" || m == "thinkertoy.txt" {
+		return "thinkertoy.txt"
+	} else {
+		return "standard.txt"
+	}
+}
+
+func filenameExist(m string) bool {
+	return m == "shadow.txt" || m == "thinkertoy.txt" || m == "standard.txt" || m == "shadow" || m == "thinkertoy" || m == "standard"
 }
